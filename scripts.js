b5ea1c7d3250fc879cd151ee907a1dfe744bc8f5
plom <plom.q8@gmail.com>
2020-10-10 20:08:59 +0300

    wc9 done

--- scripts.js
+++ scripts.js
@@ -7,7 +7,21 @@
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+function planetswitcher() { 
+let title = document.getElementById("caption")
+let image = document.getElementById("image")
+let body = document.getElementById("body")
 
+if(title.innerHTML == "القمر"){
+    title.innerHTML = "الشمس";
+    image.src = "./images/sun.jpg";
+    body.style.background = "#c67700";
+}else{
+    title.innerHTML = "القمر";
+    image.src = "./images/moon.jpg";
+    body.style.background = "#182848";
+} 
+}
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
 
